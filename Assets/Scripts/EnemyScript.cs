459102cfcf9eefc168b01218f75be66aa139f559
RHSMcLain <amclain@riverdale.k12.or.us>
2025-10-29 13:41:33 -0700

    add Attack Delay andTimer

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,13 +3,16 @@ using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
-    GameObject player;
+    [SerializeField]
+    float attackDelay = 1f;
+    float attackTimer = 0f;
+    PlayerMovement player;
     NavMeshAgent agent;
     //TODO: Get the player by its type instead, and when I hit it, call its takeDamage function
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = FindFirstObjectByType<PlayerMovement>();
         agent = GetComponent<NavMeshAgent>();
 
     }
@@ -17,7 +20,15 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        attackTimer += Time.deltaTime;
+
         agent.SetDestination(player.transform.position);
+        if (agent.remainingDistance <= 2f && attackTimer >= attackDelay)
+        {
+            player.takeDamage(1);
+            print("attack");
+            attackTimer = 0f;
+        }
 
     }
 }
