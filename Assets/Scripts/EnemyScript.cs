11dc0ad0caedfcaead121c4e28f577f999477c60
RHSMcLain <amclain@riverdale.k12.or.us>
2025-10-30 14:21:53 -0700

    adding a raycast to prove appropriate distance and line of sight for enemy to attaack

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,7 +4,9 @@ using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField]
-    float attackDelay = 1f;
+    float attackDelay = 1f, attackDistance = 2f;
+    [SerializeField]
+    Vector3 destination;
     float attackTimer = 0f;
     PlayerMovement player;
     NavMeshAgent agent;
@@ -22,13 +24,29 @@ public class EnemyScript : MonoBehaviour
     {
         attackTimer += Time.deltaTime;
 
-        agent.SetDestination(player.transform.position);
-        if (agent.remainingDistance <= 2f && attackTimer >= attackDelay)
+        bool hasDestination = agent.SetDestination(player.transform.position);
+        print(hasDestination);
+
+        destination = agent.pathEndPosition;
+        if (hasDestination && agent.remainingDistance <= 2f && attackTimer >= attackDelay)
         {
-            player.takeDamage(1);
-            print("attack");
-            attackTimer = 0f;
+            //Check to see if we are close enough to the target and can see it
+            Vector3 direction = player.transform.position - transform.position;
+
+            RaycastHit hit;
+            
+            if (Physics.Raycast(transform.position, direction, out hit,Mathf.Infinity)){
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    print("We can attack!!!");
+                    player.takeDamage(1);
+                    print("attack");
+                    attackTimer = 0f;
+                }
+            }
+            
         }
+    
 
     }
 }
